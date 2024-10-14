ebc59804b0e811b60069b872996c666022216334
Maksym-Yaroshenko <yiaroshenko.max@gmail.com>
2024-10-14 15:53:11 +0300

    fix

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,9 @@
 import css from "./Layout.module.css";
-import PropTypes from "prop-types";
 import Header from "../Header/Header.jsx";
 // import { useSelector } from "react-redux";
 // import Loader from "../Loader/Loader";
 
+// eslint-disable-next-line react/prop-types
 export default function Layout({ children }) {
   return (
     <div className={css.container}>
@@ -13,7 +13,3 @@ export default function Layout({ children }) {
     </div>
   );
 }
-
-Layout.propTypes = {
-  children: PropTypes.node,
-};
